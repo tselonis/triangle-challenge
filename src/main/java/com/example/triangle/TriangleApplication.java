ff21e731e2ba5d320dc5990773d2a15d0e722e3e
Alexandros Tselonis <alexandros.tselonis@camelotglobal.com>
2018-03-21 22:02:25 +0200

    Initial commit for Spring boot Web app

---
+++ src/main/java/com/example/triangle/TriangleApplication.java
@@ -0,0 +1,12 @@
+package com.example.triangle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriangleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriangleApplication.class, args);
+	}
+}
