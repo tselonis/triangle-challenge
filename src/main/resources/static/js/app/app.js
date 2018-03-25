db9148372197e76538eed56862afafffddf1053c
Alexandros Tselonis <alexandros.tselonis@camelotglobal.com>
2018-03-25 19:54:32 +0300

    minor enhancements

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -4,8 +4,11 @@ triangleApp.constant("restApiContextPath", "/triangle");
 
 triangleApp.controller("mainController", function ($scope, $http, restApiContextPath) {
 
-    var evaluateTypeUrl = "/type/evaluate?";
-    var queryParamSeparator = "&";
+    var EVALUATE_TYPE_URL = "/type/evaluate?";
+    var QUERY_PARAM_SEPARATOR = "&";
+    var CSS_CLASS_SUCCESS = "alert-success";
+    var CSS_CLASS_INFO = "alert-info";
+    var CSS_CLASS_DANGER = "alert-danger";
 
     $scope.init = function () {
         $scope.triangleSides = {};
@@ -16,20 +19,20 @@ triangleApp.controller("mainController", function ($scope, $http, restApiContext
 
         $scope.showOutcome = false;
 
-        var resourceUrl = evaluateTypeUrl
-            + "a=" + triangleSides.side_a + queryParamSeparator
-            + "b=" + triangleSides.side_b + queryParamSeparator
+        var resourceUrl = EVALUATE_TYPE_URL
+            + "a=" + triangleSides.side_a + QUERY_PARAM_SEPARATOR
+            + "b=" + triangleSides.side_b + QUERY_PARAM_SEPARATOR
             + "c=" + triangleSides.side_c;
 
         $http.get(restApiContextPath + resourceUrl).then(
             function successCallback(response) {
                 $scope.showOutcome = true;
-                $scope.outcomeClass = response.data.triangle ? "alert-success" : "alert-info";
+                $scope.outcomeClass = response.data.triangle ? CSS_CLASS_SUCCESS : CSS_CLASS_INFO;
                 $scope.outcomeMessage = response.data.description;
             },
             function errorCallback(response) {
                 $scope.showOutcome = true;
-                $scope.outcomeClass = "alert-danger";
+                $scope.outcomeClass = CSS_CLASS_DANGER;
                 if (response.status === 422) {
                     $scope.outcomeMessage = response.data.message;
                 } else {
