e43df57b96446770b9dafbffa54797e7b1d0c5e7
Alexandros Tselonis <alexandros.tselonis@camelotglobal.com>
2018-03-25 13:00:54 +0300

    Introduce home page

---
+++ src/main/java/com/example/triangle/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.triangle.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Model model) {
+        model.addAttribute("title", "Welcome To Triangle Application");
+        return "home";
+    }
+
+}
